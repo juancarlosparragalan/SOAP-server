4c62a08457e1abb51eab09a16d11a21143b33330
Juan Carlos Parra Galan <juancarlosparragalan@hotmail.com>
2022-12-12 12:30:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require("express"),
     xmlparser = require('express-xml-bodyparser'),
     port = process.env.PORT || 8080;
 
-
-
 const app = express(),
     server = require('http').Server(app),
     responser = require('./responser/responser');
@@ -13,7 +11,6 @@ app.use(xmlparser());
 app.use(cors());
 
 app.use('/api/v1/semillero', responser)
-
 
 server.listen(port, () => {
     console.log("Server Starts at port " + port);
